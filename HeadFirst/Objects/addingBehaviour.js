392730b5c1bf60d678c2e185b7876f6e143bba2f
Pranav <Pranav@pc-134-100.customer.prince.clv.lan>
2018-08-21 00:32:11 +0100

    Iterating through properties in an object

--- HeadFirst/Objects/addingBehaviour.js
+++ HeadFirst/Objects/addingBehaviour.js
@@ -8,16 +8,17 @@ var fiat = {
   started: false,
 
   // defining a method
+  // don't forget the commas!
   start: function()
   {
     // 'this' refers to properties in THIS very object
     this.started = true
-  }
+  },
 
   stop: function()
   {
     this.started = false
-  }
+  },
 
   drive: function()
   {
@@ -32,4 +33,17 @@ var fiat = {
 
   }
 
+}
+
+
+
+// another way to access the properties in an object
+for (var prop in fiat)
+{
+  console.log(prop + ": " + fiat.prop);
+
+  console.log("Logging the exact same thing a different way");
+
+  // exactly the same as:
+  console.log(prop + ": " + fiat[prop]);
 }
