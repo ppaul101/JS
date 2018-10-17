8efb5dc376b2bc309a905b9ce9e326d923067ea2
PP <pp051000@gmail.com>
2018-10-17 23:03:13 +0100

    Fixed horizontal jumping range and implemented Game Over detection

--- Doodle Jump/doodleJump.js
+++ Doodle Jump/doodleJump.js
@@ -7,58 +7,79 @@ if (canvas == null)
 
 // tool to paint on the canvas
 var ctx = canvas.getContext("2d")
+ctx.fillStyle = "#110D8F";
+ctx.rect(0,0,canvas.width, canvas.height)
+ctx.fill()
 
-// loading doodle img
+
+// loading images
+var background = new Image()
+background.src = "background.png"
 var doodle = new Image();
 doodle.src = "doodle.png";
+var platform = new Image()
+platform.src = "platform.png"
+
 
 // doodle initial position
-var x = canvas.width/2
+var x = (canvas.width - 220)/2
 var y = canvas.height - 300
 var dy = -10
 var initialSpeed = dy
 var doodle_dx = 8
 
+
+// for platform
+var platformHeight = 40
+var platformWidth = 140
+var platformX = (canvas.width - platformWidth)/2
+var platformY = canvas.height - platformHeight
+
+
+
 // for keyboard entry
 var rightPressed = false
 var leftPressed = false
 
+var t = true
 
 function draw()
 {
+  if (t)
+  {
+  console.log(doodle.width)
+  t = false
+  }
+
   // clear previous frame - illusion of movement
   ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-  drawDoodle()
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
+  drawDoodle()
+  drawPlatform()
 
 
   // reverse direction after it travels normal height
-  if (y < canvas.height/1.4 )
+  if (y < canvas.height/1.4)
   {
-    // dy = -dy
-    // dy *= 0.88
-    // dy *= 0.98
-
     // slow down and come back down
     dy += 0.4
 
   }
 
-  if (y>= canvas.height - 300)
+  if (y >= platformY - platformHeight*4.2 && x <= platformX + platformWidth/2 && x>= platformX - platformWidth)
   {
     // bounce up
     dy = initialSpeed
   }
-
-
-  // if (count >= 14 && count <= 18)
-  // {
-  //   dy -= 0.8
-  // }
-
-
-
+  if (y > canvas.height - 170)
+  {
+    // to ensure btm of doodle is touching ground
+    alert("game over")
+    // restart game
+    document.location.reload()
+  }
 
 
   if (leftPressed && x>0)
@@ -85,6 +106,12 @@ function drawDoodle()
   ctx.closePath()
 }
 
+function drawPlatform()
+{
+  ctx.beginPath()
+  ctx.drawImage(platform, platformX, platformY, platformWidth, platformHeight)
+  ctx.closePath()
+}
 
 
 
